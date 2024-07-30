24a1d0b2ac655e3346afe843a064466361c12bf0
Ana Mayordomo <anamayordomo@Anas-Laptop.localdomain>
2024-07-30 15:43:50 -0500

    Rep counting for curls with left arm and display on window

--- src/main.py
+++ src/main.py
@@ -11,6 +11,10 @@ mp_pose = mp.solutions.pose             # import pose estimation module (as oppo
 # video feed
 cap = cv2.VideoCapture(1)   # set up video capture device - 0 indicates webcam, this may be different for different devices
 
+# Curl counter variables
+counter = 0 
+stage = None
+
 # handle unopened camera
 if not cap.isOpened():
     print("Cannot open webcam")
@@ -62,13 +66,37 @@ with mp_pose.Pose() as pose:
             
             angle = calculate_angle(shoulder, elbow, wrist)
 
-            print(angle)
-            
+            # Curl counter logic
+            if angle > 160:
+                stage = "down"
+            if angle < 30 and stage =="down":
+                stage="up"
+                counter +=1
+                print(counter)
+
         except:
             pass
 
+        # Render curl counter
+        #   Setup status box
+        cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
+        
+        #   Rep data
+        cv2.putText(image, 'REPS', (15,12), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(counter), 
+                    (10,60), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+        
+        #   Stage data
+        cv2.putText(image, 'STAGE', (65,12), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, stage, 
+                    (60,60), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+        
 
-        #   Render detections
+        # Render detections
         dot_formatting = mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2)
         line_formatting = mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS, dot_formatting, line_formatting)
