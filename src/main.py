927cffa64e4cde7b1254ba0de1555c83891d21f0
Ana Mayordomo <anamayordomo@Anas-Laptop.localdomain>
2024-07-30 14:52:02 -0500

    Makepose detections and draw on displayed frame

--- src/main.py
+++ src/main.py
@@ -15,26 +15,51 @@ if not cap.isOpened():
     print("Cannot open webcam")
     exit()
 
-# main loop to read and display frames
-while cap.isOpened():
-    ret, frame = cap.read() # get current read from webcam // ret = return variable, frame = image from webcam
+# setup mediapipe instance
+with mp_pose.Pose() as pose:
+    i = 0
+    # main loop to read and display frames
+    while cap.isOpened():
+        ret, frame = cap.read() # get current read from webcam // ret = return variable, frame = image from webcam
 
-    # check camera availability
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+        # check camera availability
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
 
-    # check frame validity - if not valid, skip current iteration and try to read next frame
-    if frame is None or frame.size==0:
-        continue
+        # check frame validity - if not valid, skip current iteration and try to read next frame
+        if frame is None or frame.size==0:
+            continue
 
-    # display frame
-    cv2.imshow('Mediapipe Feed', frame) 
+        # Detect stuff and render
+        #   Recolor image - when pass image to mediapipe, need it in RGB as opposed to CV2's default BGR
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False     # helps save memory   
+       
+        #   Make detection
+        results = pose.process(image)
 
-    # exit loop if 'q' pressed
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
+        #   Recolor back to BGR so can render
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        #   Render detections
+        dot_formatting = mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2)
+        line_formatting = mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS, dot_formatting, line_formatting)
+        
+        if i==10:
+            print(results.pose_landmarks)
+        i+=1
+
+        # display frame
+        cv2.imshow('Mediapipe Feed', image) 
+
+        # exit loop if 'q' pressed
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
 
 cap.release()
 cv2.destroyAllWindows() # close video feed
+
 
